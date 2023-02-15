cb94702e8ab3e45081ba4c36295f2e57a8f25e3e
Samuelalex16 <samuelalexander2156@gmail.com>
2023-02-15 20:05:53 +0530

    initial commit

--- streamhttpexample.js
+++ streamhttpexample.js
@@ -2,6 +2,13 @@ var http = require('http')
 var fs = require('fs')
 
 http.createServer(function(req, res) {
-    const text = fs.readFileSync('./test/big.txt', 'utf-8')
-    return res.end(text)
+    // const text = fs.readFileSync('./test/big.txt', 'utf-8')
+    // return res.end(text)
+    const fileStream = fs.createReadStream('./test/big.txt', 'utf-8')
+    fileStream.on('open', () => {
+        fileStream.pipe(res)
+    })
+    fileStream.on('error', () => {
+        res.end(err)
+    })
 }).listen(5000)
